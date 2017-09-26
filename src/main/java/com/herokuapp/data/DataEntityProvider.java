3009f9b1a7a244e5561eaab5e76837c4161de036
Craig Stockton <cstockto.local@AGLPC0458CD.allegisgroup.com>
2017-09-26 08:15:03 -0700

    Added note concerning implementation of repositories

--- src/main/java/com/herokuapp/data/DataEntityProvider.java
+++ src/main/java/com/herokuapp/data/DataEntityProvider.java
@@ -6,6 +6,13 @@ public class DataEntityProvider {
     }
 
     public DataEntity get(DataEntityDescription description) {
+        /*  This would eventually be replaced by use of Repositories (representations of data sources).
+            The Repository implementations would include connections/queries of databases,
+            use of web services, or use of an application UI.
+            That leaves this class to be responsible for composing the requested data,
+            possibly from multiple repositories.
+            The repositories are responsible for how the data is acquired.
+         */
         return DataEntity.getInstance(description.getCheckbox1Selections(),description.getCheckbox2Selections());
     }
 }
