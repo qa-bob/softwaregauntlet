af4654bf1065ed1dfb68c7d759d72994de04e301
Craig Stockton <cstockto.local@AGLPC0458CD.allegisgroup.com>
2017-11-20 12:33:00 -0600

    Add validation of Move Card functionality

---
+++ src/test/java/com/trello/view/card/move/CardMoveTest.java
@@ -0,0 +1,26 @@
+package com.trello.view.card.move;
+
+import com.softwareonpurpose.gauntlet.GauntletTest;
+import com.trello.data.card.TrelloCard;
+import com.trello.data.card.TrelloCardDefinition;
+import com.trello.data.card.TrelloCardProvider;
+import com.trello.data.card.TrelloCardValidator;
+import com.trello.data.user.TrelloUser;
+import com.trello.data.user.TrelloUserProvider;
+import com.trello.view.login.LoginView;
+import org.testng.annotations.Test;
+
+@Test(groups={GauntletTest.Application.TRELLO, GauntletTest.View.CARD_MOVE})
+public class CardMoveTest extends GauntletTest{
+    @Test(groups={TestType.EVT})
+    public void smoke(){
+        TrelloCard testCard = TrelloCardProvider.getInstance().get();
+        String expectedList = "Done";
+        TrelloCardDefinition expected = testCard.toDefinition().withInList(expectedList);
+        TrelloUser user = TrelloUserProvider.getInstance().get();
+        LoginView.directNav().login(user);
+        CardMoveModal.directNav().move(expectedList);
+        TrelloCard actual = TrelloCardProvider.getInstance().get(testCard);
+        then(TrelloCardValidator.getInstance(expected, actual).validate());
+    }
+}
