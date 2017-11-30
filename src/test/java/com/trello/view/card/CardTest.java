838d97daf266efa266283731ecaba7a9a4e332c8
Craig A. Stockton <craig.a.stockton@gmail.com>
2017-11-29 22:43:01 -0600

    Add test data provider

--- src/test/java/com/trello/view/card/CardTest.java
+++ src/test/java/com/trello/view/card/CardTest.java
@@ -3,22 +3,29 @@ package com.trello.view.card;
 import com.softwareonpurpose.gauntlet.GauntletTest;
 import com.trello.data.TestDataProvider;
 import com.trello.data.card.TrelloCard;
+import com.trello.data.card.TrelloCardDefinition;
 import com.trello.data.user.TrelloUser;
 import com.trello.data.user.TrelloUserDefinition;
 import com.trello.data.user.TrelloUserRepository;
 import com.trello.view.login.LoginView;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @Test(groups = {GauntletTest.Application.TRELLO, GauntletTest.View.CARD})
 public class CardTest extends GauntletTest {
-    @Test(groups = {TestType.EVT})
-    public void smoke() {
+    @DataProvider
+    public static Object[][] smokeScenarios() {
+        return new Object[][]{{TrelloCardDefinition.getInstance()}};
+    }
+
+    @Test(groups = {TestType.EVT}, dataProvider = "smokeScenarios")
+    public void smoke(TrelloCardDefinition testCardDefinition) {
         TrelloUserDefinition userDefinition = TrelloUserDefinition.getInstance();
         TrelloUser user = TrelloUserRepository.getInstance().query(userDefinition);
-        TrelloCard card = TestDataProvider.getInstance().get();
+        TrelloCard card = TestDataProvider.getInstance().get(testCardDefinition);
         CardViewExpected expected = CardViewExpected.getInstance(user, card);
-        given(user);
-        given(card);
+        given(userDefinition);
+        given(testCardDefinition);
         when();
         LoginView.directNav().login(user);
         CardView actual = CardView.directNav(card);
